ff8f516c932a8f48e423d3baf728ca9d8413dc55
chrisjose <christopherjose707@gmail.com>
2020-12-28 07:52:29 -0800

    Move route file in app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI; 
-const users = require("./routes/api/users");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 
@@ -15,18 +14,20 @@ if (process.env.NODE_ENV === "production") {
 }
 
 mongoose
-  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Connected to MongoDB successfully!!"))
-  .catch((err) => console.log(err))
+.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log("Connected to MongoDB successfully!!"))
+.catch((err) => console.log(err))
 ;
 
-app.use(bodyParser.urlencoded({ extended: false })); // ORDER OF THIS MATTERS
-app.use(bodyParser.json()); // ORDER OF THIS MATTERS
+app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.json()); 
 app.use(passport.initialize());
 require("./config/passport")(passport);
 
-app.get("/", (req, res) => res.send("Hello"));
+// ADD BACKEND ROUTES HERE
+const users = require("./routes/api/users");
 app.use("/api/users", users);
+// ADD BACKEND ROUTES HERE
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
