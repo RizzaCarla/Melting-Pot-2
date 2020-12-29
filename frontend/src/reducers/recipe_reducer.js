a80c631eaee40d3b99958c91bd9623fd5c72f939
kevin park <kevin.park02@gmail.com>
2020-12-29 11:53:33 -0800

    Set the slice of state correctly for fetching all recipes

--- frontend/src/reducers/recipe_reducer.js
+++ frontend/src/reducers/recipe_reducer.js
@@ -1,21 +1,20 @@
 import { RECEIVE_ALL_RECIPES, RECEIVE_USER_RECIPES, RECEIVE_RECIPE, REMOVE_RECIPE } from "../actions/recipe_actions";
 
-const recipeReducer = (
-    state = { all: {}, user: {}, new: undefined },
-    action
-) => {
+const recipeReducer = ( state = {}, action) => {
     Object.freeze(state);
     let newState = Object.assign({}, state);
 
     switch (action.type) {
         case RECEIVE_ALL_RECIPES:
-            newState.all = action.recipes.data;
+            let i;
+            for(i = 0; i < action.recipes.data.length; i++) {
+                newState[action.recipes.data[i]._id] = action.recipes.data[i];
+            }
             return newState;
         case RECEIVE_USER_RECIPES:
             newState.user = action.recipes.data;
             return newState;
-        case RECEIVE_RECIPE:
-            newState.new = action.recipe.data;
+        case RECEIVE_RECIPE: 
             return newState;
         case REMOVE_RECIPE:
             delete newState.user[action.recipeId];
