a3be65446bddf45b5c8e917622f027fd388193b3
kevin park <kevin.park02@gmail.com>
2020-12-30 11:43:46 -0800

    Make styling changes and create a placeholder page for the recipeShow page

---
+++ frontend/src/components/recipe/recipe_show.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class RecipeShow extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render(){
+        return(
+            <h1>Recipe Show Page</h1>
+        )
+    }
+}
+
+export default RecipeShow;
---
+++ frontend/src/components/recipe/recipe_show_container.jsx
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+import RecipeShow from './recipe_show';
+
+const mapStateToProps = (state) => {
+    return({
+
+    });
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return({
+        
+    })
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(RecipeShow);
