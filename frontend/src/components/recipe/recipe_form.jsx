8047f11da561a5837513ad58e543c2aa1e4940ad
kevin park <kevin.park02@gmail.com>
2020-12-29 19:45:51 -0800

    Make correct changes to the form

--- frontend/src/components/recipe/recipe_form.jsx
+++ frontend/src/components/recipe/recipe_form.jsx
@@ -5,35 +5,66 @@ class RecipeForm extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            authorId: this.props.currentUser.id,
+            authorId: this.props.currentUser._id,
             name: "",
             story: "",
-            ingredients: ["hello", "my", "name"],
-            instructions: [],
+            ingredients: this.props.ingredients,
+            instructions: this.props.instructions,
             cookingTime: "",
             difficulty: "",
             category: ""
         }
     }
 
+    update(field) {
+        return e => this.setState({[field]: e.target.value})
+    }
+
+    handleIngredient(e) {
+        e.preventDefault();
+        
+    }
+
     render() {
+        if(this.props.currentUser === undefined) {
+            return null;
+        }
         return(
             <form className="recipe-form-cont">
                 <div className="recipe-top">
                     <div className="recipe-pic-name">
                         <input type="file"/>
-                        <input type="text"/>
+                        <input type="text"
+                               onChange={this.update("name")}
+                               placeholder="Name of your recipe"/>
                     </div>
                     <div className="recipe-info">
                         <label>Difficulty:
-                            <select>
+                            <select onChange={this.update("difficulty")}>
+                                <option value="" selected disabled>Please select</option>
                                 <option value="easy">easy</option>
                                 <option value="medium">medium</option>
                                 <option value="hard">hard</option>
                             </select>
                         </label>
                         <label>Cooking Time:
-                            <input type="text"/>
+                            <select onChange={this.update("cookingTime")}>
+                                <option value="" selected disabled>Please select</option>
+                                <option value="0.5 hr">0.5 hr</option>
+                                <option value="1 hr">1 hr</option>
+                                <option value="1.5 hr">1.5 hr</option>
+                                <option value="2+ hrs">2+ hrs</option>
+                            </select>
+                        </label>
+                        <label>Category:
+                            <select onChange={this.update("category")}>
+                                <option value="" selected disabled>Please select</option>
+                                <option value="Meat">Meat</option>
+                                <option value="Vegetables">Vegetables</option>
+                                <option value="Poultry">Poultry</option>
+                                <option value="Carbs">Carbs</option>
+                                <option value="Noodle">Noodle</option>
+                            </select>
                         </label>
                     </div>
                 </div>
@@ -41,15 +72,16 @@ class RecipeForm extends React.Component {
                     <div className="ingredients">
                         <h3>Ingredients:</h3>
                         <ul>
-                            {this.state.ingredients.map(ingredient => {
-                                return(<li>{ingredient}</li>)
+                            {this.state.ingredients.map((ingredient,idx) => {
+                                return(<li key={idx}>{ingredient}</li>)
                             })}
                         </ul>
                         <input type="text"
-                            placeholder="write ingredients"/>
+                               placeholder="write ingredients"
+                               onChange={this.handleIngredient}/>
                     </div>
                     <div className="recipe-owner">
-                        <h3>Recipe Owner:</h3>
+                        <h3>Recipe Owner: &nbsp;<span>{this.props.currentUser.handle}</span></h3>
                         <div className="recipe-owner-pic"></div>
                     </div>
                 </div>
@@ -61,6 +93,7 @@ class RecipeForm extends React.Component {
                     </div>
                     <div className="recipe-story">
                         <textarea cols="26" rows="10"
+                                  onChange={this.update("story")}
                                   placeholder="write your story about this recipe"/>
                     </div>
                 </div>
@@ -70,3 +103,4 @@ class RecipeForm extends React.Component {
 }
 
 export default RecipeForm
+
