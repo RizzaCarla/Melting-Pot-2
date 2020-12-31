f4bbdce69d7a3c6c5d5971ced8e4aec0c40d1df3
kevin park <kevin.park02@gmail.com>
2020-12-30 17:12:50 -0800

    Incorporate all the changes

---
+++ frontend/src/components/recipe/recipe_show_container.jsx
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux';
+import { getRecipes, getRecipe, getUserRecipes, updateRecipe, deleteRecipe } from '../../actions/recipe_actions';
+import RecipeShow from './recipe_show';
+
+const mapStateToProps = (state, ownProps) => {
+    return({
+        recipe: state.entities.recipes[ownProps.match.params.recipeId]
+    });
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return({
+        getRecipes: () => dispatch(getRecipes()),
+        getRecipe: (recipeId) => dispatch(getRecipe(recipeId)),
+        getUserRecipes: (authorId) => dispatch(getUserRecipes(authorId)),
+        updateRecipe: (recipe) => dispatch(updateRecipe(recipe)),
+        deleteRecipe: (recipeId) => dispatch(deleteRecipe(recipeId))
+    })
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(RecipeShow);
