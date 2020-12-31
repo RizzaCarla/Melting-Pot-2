93c430a0bee91e14cfed7d6e84cc05f4231d3400
rizzacarla <rizzacarla.12@gmail.com>
2020-12-30 17:09:01 -0800

    Finish routes for likes

--- routes/api/likes.js
+++ routes/api/likes.js
@@ -1,10 +1,38 @@
+
 const express = require("express");
 const mongoose = require('mongoose');
 const router = express.Router();
 const validateLikeInput = require('../../validation/likes');
 const Like = require('../../models/Like');
 
+//CREATE LIKE
+router.post('/new', (req, res) => {
+  const { errors, isValid } = validateLikeInput(req.body)
+
+  if (!isValid) {
+    return res.status(404).json(errors);
+  }
+
+  const newLike = new Like({
+    authorId: req.body.authorId,
+    recipeId: req.body.recipeId
+  })
+
+  newLike.save()
+    .then(like => res.json(like))
+})
+
 //UPDATE LIKES
-// router.post
+router.patch('/edit/:id', (req, res) => {
+  mongoose.set('useFindAndModify', false);
+  const { errors, isValid } = validateLikeInput(req.body)
+
+  if (!isValid) {
+    return res.status(404).json(errors);
+  }
+
+  Like.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then(like => res.json(like))
+})
 
 module.exports = router;
