f06aa11d429b026871fdde43f274425ae6017b54
kevin park <kevin.park02@gmail.com>
2020-12-31 01:37:13 -0800

    Format edit form

--- frontend/src/components/recipe/recipe_edit.jsx
+++ frontend/src/components/recipe/recipe_edit.jsx
@@ -4,8 +4,9 @@ class RecipeEdit extends React.Component {
     constructor(props){
         super(props)
         this.state = this.props.recipe;
-        console.log("this is state");
-        console.log(this.state);
+
+        this.update = this.update.bind(this);
+        this.handleInstruction = this.handleInstruction.bind(this);
     }
 
     componentDidMount(){
@@ -18,6 +19,17 @@ class RecipeEdit extends React.Component {
         }
     }
 
+    update(field) {
+        return e => this.setState({[field]: e.target.value})
+    }
+
+    handleInstruction(e) {
+        e.preventDefault();
+        console.log(e);
+        // const oldInstructions = this.state.instructions;
+        // this.setState({[instructions]: this.state.instructions[]})
+    }
+
     render() {
         const recipe = this.props.recipe;
 
@@ -26,49 +38,61 @@ class RecipeEdit extends React.Component {
         }
         return(
             <div>
-                <h1>Edit</h1>
-                {this.state.authorId}
-                {this.state.name}
-                {this.state.story}
+                <label>Recipe Name:
+                    <input type="text"
+                        value={this.state.name}
+                        placeholder={recipe.name}
+                        onChange={this.update("name")}/>
+                </label>
+                <label>Difficulty:
+                    <select onChange={this.update("difficulty")} value={this.state.difficulty}>
+                        <option value="easy">Easy</option>
+                        <option value="medium">Medium</option>
+                        <option value="hard">Hard</option>
+                    </select>
+                </label>
+                <label>Cooking Time:
+                    <select onChange={this.update("cookingTime")} value={this.state.cookingTime}>
+                        <option value="0.5 hr">0.5 hr</option>
+                        <option value="1 hr">1 hr</option>
+                        <option value="1.5 hr">1.5 hr</option>
+                        <option value="2+ hrs">2+ hr</option>
+                    </select>             
+                </label>
+                <label>Category:
+                    <select onChange={this.update("category")} value={this.state.category}>
+                        <option value="Meat">Meat</option>
+                        <option value="Vegetables">Vegetables</option>
+                        <option value="Poultry">Poultry</option>
+                        <option value="Carbs">Carbs</option>
+                        <option value="Noodle">Soup</option>
+                    </select>
+                </label>
+                <label>Likes: {recipe.numLikes}</label>
+                <label>Recipe Owner:
+                    <h5>Author Name(Placeholder)</h5>
+                </label>
+                <label>Story:
+                    <textarea cols="26" rows="10"
+                              value={this.state.story}
+                              onChange={this.update("story")}
+                              placeholder="Write your story about this recipe"/>
+                </label>
+                <label>Instructions:
+                    <ul>
+                        {this.state.instructions.map((instruction, idx) => {
+                            return(
+                                <li key={idx}>
+                                    <input type="text"
+                                            value={this.state.instructions[idx]}
+                                            placeholder={instruction}
+                                            onChange={this.handleInstruction}/>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </label>
             </div>
-            // <div className="recipe-show-container">
-            //     <div className="recipe-show-info">
-            //         <div className="recipe-pic">
-            //             <div className="recipe-pic"></div>
-            //             {recipe.name}
-            //         </div>
-            //         <div className="recipe-misc-info">
-            //             <ul>
-            //                 <li>Difficulty:&nbsp;&nbsp;{recipe.difficulty}</li>
-            //                 <li>Cooking Time:&nbsp;&nbsp;{recipe.cookingTime}</li>
-            //             </ul>
-            //         </div>
-            //     </div>
-            //     <div className="recipe-detail-info">
-            //         <div className="recipe-show-ing-inst">
-            //             <h4>Igredients:</h4>
-            //             <ul className="recipe-ing-list">
-            //                 {recipe.ingredients.map((ingredient, idx) => {
-            //                     return(<li key={idx}>{ingredient}</li>)
-            //                 })}
-            //             </ul>
-            //             <h4>Instructions: </h4>
-            //             <ul className="recipe-inst-list">
-            //                 {recipe.instructions.map((instruction, idx) => {
-            //                     return(<li key={idx}>{instruction}</li>)
-            //                 })}
-            //             </ul>
-            //         </div>
-            //         <div className="recipe-owner-info">
-            //             <div className="recipe-show-owner">
-            //                 <div className="owner-pic"></div>
-            //                 {/* {recipe.authorId} */}
-            //             </div>
-            //             <h4>Recipe Story:</h4>
-            //             {recipe.story}
-            //         </div>
-            //     </div>
-            // </div>
         )
     }
 }
