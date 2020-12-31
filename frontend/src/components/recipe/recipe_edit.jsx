f3dd7ca31da87b29969a456cf41cef056d5d90f3
kevin park <kevin.park02@gmail.com>
2020-12-30 17:43:37 -0800

    Start recipe edit container

---
+++ frontend/src/components/recipe/recipe_edit.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class RecipeEdit extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    render() {
+        return(
+            <h1>Recipe Edit Page</h1>
+        )
+    }
+}
+
+export default RecipeEdit;
---
+++ frontend/src/components/recipe/recipe_edit_container.jsx
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+import RecipeEdit from './recipe_edit';
+
+const mapStateToProps = (state, ownProps) => {
+    return({
+
+    })
+}
+
+const mapDispatchToProp = (dispatch) => {
+    return({
+
+    })
+}
+
+export default connect(mapStateToProps, mapDispatchToProp)(RecipeEdit);
