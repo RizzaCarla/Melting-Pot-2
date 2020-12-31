7e8a85808dad971fff5f842eb7d257f1e229c8ac
chrisjose <christopherjose707@gmail.com>
2020-12-30 21:05:47 -0800

    gitignore keys_dev

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -2,9 +2,10 @@ module.exports = {
   mongoURI:
     "mongodb+srv://CJ:kOFDSoHsqWB0kZjn@cluster0.wumml.mongodb.net/<dbname>?retryWrites=true&w=majority",
   secretOrKey: "secret",
-  AWS_ACCESS_KEY_ID: "AKIAZLJASQPEDRIKNFQL",
-  AWS_SECRET_ACCESS_KEY: "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",
+  AWS_ACCESS_KEY_ID: "AKIAZLJASQPECAPVUGNF",
+  AWS_SECRET_ACCESS_KEY: "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",
   AWS_BUCKET_NAME: "meltingpot-mern",
   AWS_REGION: "us-west-1",
-  AWS_Uploaded_File_URL_LINK: "https://s3-us-west-1.amazonaws.com/meltingpot-mern"
+  AWS_Uploaded_File_URL_LINK:
+    "https://s3-us-west-1.amazonaws.com/meltingpot-mern",
 };
