b639e490a6783b85a11b24df4401a0612453f0d2
chrisjose <christopherjose707@gmail.com>
2021-01-01 19:57:33 -0800

    Add styling for recipe edit

--- frontend/src/components/recipe/recipe_edit.jsx
+++ frontend/src/components/recipe/recipe_edit.jsx
@@ -19,6 +19,7 @@ class RecipeEdit extends React.Component {
     }
 
     componentDidMount(){
+        this.props.getRecipes();
         this.props.getUserRecipes(this.props.currentUser._id);
     }
     
@@ -100,86 +101,113 @@ class RecipeEdit extends React.Component {
         }
         return(
             <div className="recipe-edit-parent">
-                <label>Recipe Name:
-                    <input type="text"
-                        value={this.state.name}
-                        placeholder={recipe.name}
-                        onChange={this.update("name")}/>
-                </label>
-                <label>Difficulty:
-                    <select onChange={this.update("difficulty")} value={this.state.difficulty}>
-                        <option value="easy">Easy</option>
-                        <option value="medium">Medium</option>
-                        <option value="hard">Hard</option>
-                    </select>
-                </label>
-                <label>Cooking Time:
-                    <select onChange={this.update("cookingTime")} value={this.state.cookingTime}>
-                        <option value="0.5 hr">0.5 hr</option>
-                        <option value="1 hr">1 hr</option>
-                        <option value="1.5 hr">1.5 hr</option>
-                        <option value="2+ hrs">2+ hr</option>
-                    </select>             
-                </label>
-                <label>Category:
-                    <select onChange={this.update("category")} value={this.state.category}>
-                        <option value="Meat">Meat</option>
-                        <option value="Vegetables">Vegetables</option>
-                        <option value="Poultry">Poultry</option>
-                        <option value="Carbs">Carbs</option>
-                        <option value="Noodle">Soup</option>
-                    </select>
-                </label>
-                <label>Likes: {recipe.numLikes}</label>
-                <button onClick={this.handleSubmit}>Save Recipe</button>
-                <label>Recipe Owner:
-                    <h5>Author Name(Placeholder)</h5>
-                </label>
-                <button onClick={this.handleDelete}>Delete Recipe</button>
-                <label>Story:
-                    <textarea cols="26" rows="10"
-                              value={this.state.story}
-                              onChange={this.update("story")}
-                              placeholder="Write your story about this recipe"/>
-                </label>
-                <label>Ingredients:
-                    <ul>
-                        {this.state.ingredients.map((ingredient, idx) => {
-                            return(
-                                <li key={idx}>
+                <div className="recipe-edit">
+                    <div className="recipe-edit-top">
+                        <div className="recipe-edit-top-left">
+                            <div className="recipe-edit-img-parent">
+                                <div className="recipe-edit-img">
+                                    <img src={recipe.photoUrl}></img>
+                                </div>
+                                <label className="recipe-edit-name">Recipe Name:
                                     <input type="text"
-                                            value={this.state.ingredients[idx]}
-                                            placeholder={ingredient}
-                                            onClick={() => this.handleIngClick(idx)}
-                                            onChange={this.handleIngredient}/>
-                                </li>
-                            )
-                        })}
-                    </ul>
-                    <input type="text"
-                           placeholder="Add more ingredient"
-                           onChange={this.queueIngredient}/>
-                    <button onClick={this.pushIngredient}>+</button>
-                </label>
-                <label>Instructions:
-                    <ul>
-                        {this.state.instructions.map((instruction, idx) => {
-                            return(
-                                <li key={idx}>
+                                        value={this.state.name}
+                                        placeholder={recipe.name}
+                                        onChange={this.update("name")}/>
+                                </label>
+                            </div>
+                        </div>
+                        <div className="recipe-edit-top-right">
+                            <label>Difficulty:
+                                <select onChange={this.update("difficulty")} value={this.state.difficulty}>
+                                    <option value="easy">Easy</option>
+                                    <option value="medium">Medium</option>
+                                    <option value="hard">Hard</option>
+                                </select>
+                            </label>
+                            <label>Cooking Time:
+                                <select onChange={this.update("cookingTime")} value={this.state.cookingTime}>
+                                    <option value="0.5 hr">0.5 hr</option>
+                                    <option value="1 hr">1 hr</option>
+                                    <option value="1.5 hr">1.5 hr</option>
+                                    <option value="2+ hrs">2+ hr</option>
+                                </select>             
+                            </label>
+                            <label>Category:
+                                <select onChange={this.update("category")} value={this.state.category}>
+                                    <option value="Meat">Meat</option>
+                                    <option value="Vegetables">Vegetables</option>
+                                    <option value="Poultry">Poultry</option>
+                                    <option value="Carbs">Carbs</option>
+                                    <option value="Noodle">Soup</option>
+                                </select>
+                            </label>
+                            <label>Likes: {!recipe.numLikes ? "0" : recipe.numLikes}</label>
+                            <button onClick={this.handleSubmit}>Save Recipe</button>
+                        </div>
+                    </div>
+                    <div className="recipe-edit-bottom">
+                        <div className="recipe-edit-bottom-left">
+                            <div className="recipe-edit-ingredients">
+                                <label>Ingredients:
+                                    <ul>
+                                        {this.state.ingredients.map((ingredient, idx) => {
+                                            return(
+                                                <li key={idx}>
+                                                    <input type="text"
+                                                            value={this.state.ingredients[idx]}
+                                                            placeholder={ingredient}
+                                                            onClick={() => this.handleIngClick(idx)}
+                                                            onChange={this.handleIngredient}/>
+                                                </li>
+                                            )
+                                        })}
+                                    </ul>
                                     <input type="text"
-                                            value={this.state.instructions[idx]}
-                                            placeholder={instruction}
-                                            onClick={() => this.handleInstClick(idx)}
-                                            onChange={this.handleInstruction}/>
-                                </li>
-                            )
-                        })}
-                    </ul>
-                    <input type="text"
-                           placeholder="Add more instruction"
-                           onChange={this.queueInstruction}/>
-                    <button onClick={this.pushInstruction}>+</button>
-                </label>
+                                        placeholder="Add more ingredient"
+                                        onChange={this.queueIngredient}/>
+                                    <button onClick={this.pushIngredient}>+</button>
+                                </label>
+                            </div>
+                            <div className="recipe-edit-instructions">
+                                <label>Instructions:
+                                <ul>
+                                    {this.state.instructions.map((instruction, idx) => {
+                                        return(
+                                            <li key={idx}>
+                                                <input type="text"
+                                                        value={this.state.instructions[idx]}
+                                                        placeholder={instruction}
+                                                        onClick={() => this.handleInstClick(idx)}
+                                                        onChange={this.handleInstruction}/>
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
+                                    <input type="text"
+                                        placeholder="Add more instruction"
+                                        onChange={this.queueInstruction}/>
+                                    <button onClick={this.pushInstruction}>+</button>
+                                </label>
+                            </div>
+                        </div>
+                        <div className="recipe-edit-bottom-right">
+                            <label>Recipe Owner: </label>
+                                <div className="recipe-edit-owner">
+                                    <img src={this.props.currentUser.photoUrl}></img>
+                                    <div className="recipe-edit-owner-right">
+                                        <h5>{this.props.currentUser.handle}</h5>
+                                        <button onClick={this.handleDelete}>Delete Recipe</button>
+                                    </div>
+                                </div>
+                            <label>Story:
+                                <textarea cols="26" rows="10"
+                                        value={this.state.story}
+                                        onChange={this.update("story")}
+                                        placeholder="Write your story about this recipe"/>
+                            </label>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
