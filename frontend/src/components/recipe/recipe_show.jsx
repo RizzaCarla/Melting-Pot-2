42f5fa1057f3a456fcdaa0adee5b0ed6ce8cb6c6
chrisjose <christopherjose707@gmail.com>
2021-01-12 10:48:47 -0800

    fix footer styling

--- frontend/src/components/recipe/recipe_show.jsx
+++ frontend/src/components/recipe/recipe_show.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './recipe_show.css';
 import { Link } from 'react-router-dom';
 
+
+
 import CommentShowContainer from "../comment/comment_index_container";
 
 class RecipeShow extends React.Component {
@@ -24,7 +26,6 @@ class RecipeShow extends React.Component {
         if ((this.props.recipe === undefined) || (Object.keys(this.props.authors).length === 0)) {
             return null
         }
-        
         const recipe = this.props.recipe;
         const author = this.props.authors[recipe.authorId];
 
@@ -37,58 +38,71 @@ class RecipeShow extends React.Component {
                                             <button className="user-only-btn-delete" onClick={this.handleDelete}>Delete Recipe</button>
                                         </div>
 
-        return(
-            <div className="recipe-show-container">
-                <div className="recipe-show-info">
-                    <div className="recipe-pic">
-                        <img src={recipe.photoUrl} alt=""/>
-                        <h4>{recipe.name}</h4>
-                    </div>
-                    <div className="recipe-misc-info">
-                        <ul>
-                            <li><h4>Difficulty:&nbsp;&nbsp;<span>{recipe.difficulty}</span></h4></li>
-                            <li><h4>Cooking Time:&nbsp;&nbsp;<span>{recipe.cookingTime}</span></h4></li>
-                            {/* <li><h4>Likes:&nbsp;&nbsp;<span>{recipe.numLikes === null ? 0 : recipe.numlikes}</span></h4></li> */} {/*// TODO comment this back in after figuring out how to implement likes in WK16*/}
-                            {userOnlyBtns}
-                        </ul>
-                    </div>
-                </div>
-                <div className="recipe-detail-info">
-                    <div className="recipe-show-ing-inst">
-                        <h4>Ingredients:</h4>
-                        <ul className="recipe-ing-list">
-                            {recipe.ingredients.map((ingredient, idx) => {
-                                return(<li key={idx}>{ingredient}</li>)
-                            })}
-                        </ul>
-                        <h4>Instructions: </h4>
-                        <ul className="recipe-inst-list">
-                            {recipe.instructions.map((instruction, idx) => {
-                                return(<li key={idx}>{instruction}</li>)
-                            })}
-                        </ul>
-                    </div>
-                    <div className="recipe-owner-story-info">
-                        <div className="recipe-show-owner">
-                            <div className="recipe-show-owner-personal-cont">
-                                <h3>Recipe&nbsp;Owner:</h3>
-                                <div className="recipe-show-owner-content">
-                                    <img src={author.photoUrl} alt="author-img"/>
-                                    <h4>{author.handle}</h4>
-                                </div>
-                            </div>
-                            <div className="recipe-show-owner-story-cont">
-                                <h4>Recipe Story:</h4>
-                                {recipe.story}
-                            </div>
-                        </div>
-                    </div>
-                </div> 
-                <CommentShowContainer recipe={recipe} 
-                                      authors={this.props.authors}/>
-                <Link className="recipe-show-go-back-btn-text" to="/profile"><button className="recipe-show-go-back-btn">Go Back</button></Link>
+        return (
+          <div className="recipe-show-container">
+            <div className="recipe-show-info">
+              <div className="recipe-pic">
+                <img src={recipe.photoUrl} alt="" />
+                <h4>{recipe.name}</h4>
+              </div>
+              <div className="recipe-misc-info">
+                <ul>
+                  <li>
+                    <h4>
+                      Difficulty:&nbsp;&nbsp;<span>{recipe.difficulty}</span>
+                    </h4>
+                  </li>
+                  <li>
+                    <h4>
+                      Cooking Time:&nbsp;&nbsp;<span>{recipe.cookingTime}</span>
+                    </h4>
+                  </li>
+                  {/* <li><h4>Likes:&nbsp;&nbsp;<span>{recipe.numLikes === null ? 0 : recipe.numlikes}</span></h4></li> */}{" "}
+                  {/*// TODO comment this back in after figuring out how to implement likes in WK16*/}
+                  {userOnlyBtns}
+                </ul>
+              </div>
             </div>
-        )
+            <div className="recipe-detail-info">
+              <div className="recipe-show-ing-inst">
+                <h4>Ingredients:</h4>
+                <ul className="recipe-ing-list">
+                  {recipe.ingredients.map((ingredient, idx) => {
+                    return <li key={idx}>{ingredient}</li>;
+                  })}
+                </ul>
+                <h4>Instructions: </h4>
+                <ul className="recipe-inst-list">
+                  {recipe.instructions.map((instruction, idx) => {
+                    return <li key={idx}>{instruction}</li>;
+                  })}
+                </ul>
+              </div>
+              <div className="recipe-owner-story-info">
+                <div className="recipe-show-owner">
+                  <div className="recipe-show-owner-personal-cont">
+                    <h3>Recipe&nbsp;Owner:</h3>
+                    <div className="recipe-show-owner-content">
+                      <img src={author.photoUrl} alt="author-img" />
+                      <h4>{author.handle}</h4>
+                    </div>
+                  </div>
+                  <div className="recipe-show-owner-story-cont">
+                    <h4>Recipe Story:</h4>
+                    {recipe.story}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <CommentShowContainer
+              recipe={recipe}
+              authors={this.props.authors}
+            />
+            <Link className="recipe-show-go-back-btn-text" to="/profile">
+              <button className="recipe-show-go-back-btn">Go Back</button>
+            </Link>
+          </div>
+        );
     }
 }
 
