e4dec6efe4619309ece77e422d361d8ec743542f
chrisjose <christopherjose707@gmail.com>
2021-01-14 08:16:03 -0800

    Additional footer styling

--- frontend/src/components/footer/footer.js
+++ frontend/src/components/footer/footer.js
@@ -5,21 +5,23 @@ class Footer extends React.Component {
   render() {
     return (
       <div className="footer">
-        <ul className="footer-list">
-          Developed by:
-          <li>
-            <a href="https://github.com/ChristopherJose707">Christopher Jose</a>
-          </li>
-          <li>
-            <a href="https://github.com/RizzaCarla">Rizza Marzo</a>
-          </li>
-          <li>
-            <a href="https://github.com/SanYung">San Yung</a>
-          </li>
-          <li>
-            <a href="https://github.com/kevinpark02">Kevin Park</a>
-          </li>
-        </ul>
+        <div className="footer-list-div">
+          <ul className="footer-list">
+            Developed by:
+            <li>
+              <a href="https://github.com/ChristopherJose707">Christopher Jose</a>
+            </li>
+            <li>
+              <a href="https://github.com/RizzaCarla">Rizza Marzo</a>
+            </li>
+            <li>
+              <a href="https://github.com/SanYung">San Yung</a>
+            </li>
+            <li>
+              <a href="https://github.com/kevinpark02">Kevin Park</a>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
